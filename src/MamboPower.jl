70f3232723c91780761745be5e84bc58c160c9ad
Manho Joung <MamboPower91@gmail.com>
2024-08-21 13:09:27 -0700

    initial

--- src/MamboPower.jl
+++ src/MamboPower.jl
@@ -2,4 +2,7 @@ module MamboPower
 
 # Write your package code here.
 
+export greet_your_package_name
+include("functions.jl")
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "MamboPower.jl" begin
     # Write your tests here.
+    @test MamboPower.greet_your_package_name() == "Hello MamboPower!"
+    @test MamboPower.greet_your_package_name() != "Hello world!"
 end
